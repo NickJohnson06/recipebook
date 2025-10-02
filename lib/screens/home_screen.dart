c5e287a3aa5dd8969ee7f7d6ef42a9a6c4b540a0
NickJohnson06 <acejohnny06@gmail.com>
2025-10-02 15:30:43 -0400

    displays list of recipes on HomeScreen with tappable items

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,13 +3,41 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
+  final List<Recipe> favRecipes = [
+    Recipe(
+      name: '',
+      ingredients: [],
+      instructions: '',
+    ),
+    Recipe(
+      name: '',
+      ingredients: [],
+      instructions: '',
+    ),
+    Recipe(
+      name: '',
+      ingredients: [],
+      instructions: '',
+    ),
+  ];
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('My Favorite Recipes')),
-      body: const Center(
-        child: Text('Homescreen content will go here')
+      body: ListView.builder(
+        itemCount: favRecipes.length,
+        itemBuilder: (context, index) {
+          final recipe = favRecipes[index];
+          return ListTile(
+            title: Text(recipe.name),
+            subtitle: Text(recipe.ingredients.join(', ')),
+            trailing: const Icon(Icons.arrow_forward_ios),
+            onTap: () {
+            },
+          );
+        },
       ),
     );
   }
