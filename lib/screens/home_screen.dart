ae707049230edd9d9da1a046234f21da1a65d369
NickJohnson06 <acejohnny06@gmail.com>
2025-10-03 13:06:25 -0400

    Updated recipes and centered App Bar title

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,19 +5,22 @@ import '../models/recipe.dart';
 // Recipe list
 final List<Recipe> favRecipes = [
   Recipe(
-    name: 'Example 1',
-    ingredients: ['A', 'B', 'C', 'D', 'E'],
-    instructions: 'Example instructions here',
+    name: 'Chicken Quesadilla',
+    ingredients: ['Flour tortillas', 'Cooked chicken, shredded', 'Mont Jack Cheese', 'Salsa'],
+    instructions:
+        'Place chicken and cheese on a tortilla. Top with another tortilla. Cook in a skillet until cheese melts and tortilla is golden. Serve with salsa.',
   ),
   Recipe(
-    name: 'Example 2',
-    ingredients: ['F', 'G', 'H', 'I', 'J'],
-    instructions: 'Example instructions here',
+    name: 'Avocado Toast',
+    ingredients: ['Bread slices', 'Ripe avocado', 'Lemon juice', 'Salt & pepper', 'Tomato, diced'],
+    instructions:
+        'Toast the bread slices. Mash avocado with lemon juice, salt, and pepper. Spread avocado on toast and top with diced tomatoes. Finsih it off by drizzling Balsamic Vinegarette across the top.',
   ),
   Recipe(
-    name: 'Example 3',
-    ingredients: ['K', 'L', 'M', 'N'],
-    instructions: 'Example instructions here',
+    name: 'Garlic Butter Shrimp',
+    ingredients: ['Shrimp (peeled and deveined)', 'Butter', 'Garlic, minced', 'Lemon juice', 'Salt & pepper'],
+    instructions:
+        'Melt butter in a skillet over medium heat. Add garlic and sauté until fragrant. Add shrimp and cook 2–3 minutes per side until pink. Squeeze lemon juice over shrimp and season with salt and pepper.',
   ),
 ];
 
@@ -27,7 +30,9 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('My Favorite Recipes')),
+      appBar: AppBar(title: const Text('My Favorite Recipes'),
+      centerTitle: true,
+      ),
       body: ListView.builder(
         itemCount: favRecipes.length,
         itemBuilder: (context, index) {
